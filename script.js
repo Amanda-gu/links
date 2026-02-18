8c65dad8c6780a1ab505b5a6b3caba412ad13a28
Amanda-gu <guoj826@newschool.edu>
2026-02-18 10:13:50 -0500

    attempts to get random to work

--- script.js
+++ script.js
@@ -62,16 +62,29 @@ document.addEventListener('click', (event) => {
 //from michael
 //will do later
 // Lil’ helper function to randomly sort a list (array):
-let shuffleArray = (array) => {
-	array.sort(() => Math.random() - 0.5)
-}
 
 // More on `.sort()` and `Math.random`:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-let someArray = [1, 2, 3, 4, 5] // Think of these as your blocks (`json.data`)…
 
-shuffleArray(someArray) // Shuffle the list!
 
-console.log(someArray) // Ex: [2, 1, 4, 5, 3]
+
+window.addEventListener('load', function() {
+
+	let shuffleCards = (array) => {
+		array.sort(() => Math.random() - 0.5)
+	}
+	
+	let myCards = Array.from(document.querySelectorAll('#channel-blocks li'))
+	
+	let shuffledCards = shuffleCards(myCards)
+	
+	// IMPORTANT: Update the DOM with the new order
+	shuffledCards.forEach(card => container.appendChild(card))
+
+
+	console.log(shuffledCards)
+
+
+})
